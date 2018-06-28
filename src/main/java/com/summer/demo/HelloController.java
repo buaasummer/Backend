94b677e932c33573724c95f02b29810eccf84512
alex <1219296951@qq.com>
2018-06-28 16:54:25 +0800

    项目初始部署（hello)测试

---
+++ src/main/java/com/summer/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.summer.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping (value = "/hello")
+    public String say()
+    {
+        return "hello";
+    }
+}
