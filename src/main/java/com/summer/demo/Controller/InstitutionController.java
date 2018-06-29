b91d39844796defb0a7fc20f9993d584b491c1bc
Jack2313 <570055398@qq.com>
2018-06-30 02:05:59 +0800

    enable upload files, perhaps..

--- src/main/java/com/summer/demo/Controller/InstitutionController.java
+++ src/main/java/com/summer/demo/Controller/InstitutionController.java
@@ -7,10 +7,18 @@ import com.summer.demo.Entity.InstitutionUser;
 import com.summer.demo.Repository.InstitutionUserRepository;
 import com.summer.demo.Repository.InstitutionRepository;
 import com.summer.demo.Entity.Institution;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+import javax.servlet.http.*;
 
 @CrossOrigin
 @RestController
-public class InstitutionController {
+public class InstitutionController{
+    private String filePath="C:\\Users\\Administrator\\Documents\\uploads\\";
+
     @Autowired
     private InstitutionUserRepository userRepo;
 
@@ -18,36 +26,52 @@ public class InstitutionController {
     private InstitutionRepository institutionRepo;
 
     @PostMapping(value = "/institution/register")
-    public boolean userRegister(@RequestBody CostomizedInstitution info){
+    public boolean userRegister(@RequestParam("file") MultipartFile file, HttpServletRequest request){
         //相当于新建一个单位用户和一个单位
         InstitutionUser newuser=new InstitutionUser();
         Institution newinstitution = new Institution();
 
         //昵称重复
-        if(userRepo.getNumberOfUsername(info.getUsername())>0){
+        if(userRepo.getNumberOfUsername(request.getParameter("username"))>0){
             return false;
         }else{
             //重复的单位
-            if(institutionRepo.getNumberOfInstitutionName(info.getInstitution())>0){
+            if(institutionRepo.getNumberOfInstitutionName(request.getParameter("institution"))>0){
                 return false;
             }
             //不正确的信息
             try {
-                newuser.setPassword(PasswordStorage.createHash(info.getPassword()));
+                newuser.setPassword(PasswordStorage.createHash(request.getParameter("password")));
             } catch (PasswordStorage.CannotPerformOperationException e) {
                 e.printStackTrace();
                 return false;
             }
-            newinstitution.setInstitutionName(info.getInstitution());
-            newinstitution.setDownloadUrl("");
-            newinstitution.setLegalPersonEmail(info.getEmail());
-            newinstitution.setLegalPersonName(info.getLegalPerson());
-            newinstitution.setLegalPersonPhoneNumber(info.getContact());
-            newinstitution.setOrganizationCode(info.getInstitutionCode());
-            newinstitution.setPostalAddress(info.getAddress());
-            newinstitution.setTaxPayerCode(info.getTaxpayer());
-            newuser.setUsername(info.getUsername());
+            newinstitution.setInstitutionName(request.getParameter("institution"));
+            newinstitution.setLegalPersonEmail(request.getParameter("email"));
+            newinstitution.setLegalPersonName(request.getParameter("legalPerson"));
+            newinstitution.setLegalPersonPhoneNumber(request.getParameter("contact"));
+            newinstitution.setOrganizationCode(request.getParameter("institutionCode"));
+            newinstitution.setPostalAddress(request.getParameter("address"));
+            newinstitution.setTaxPayerCode(request.getParameter("taxpayer"));
+            newuser.setUsername(request.getParameter("username"));
             newuser.setInstitution(newinstitution);
+
+            String fileName = file.getOriginalFilename();
+            // 文件后缀
+            String suffixName = fileName.substring(fileName.lastIndexOf("."));
+            // 重新生成唯一文件名，用于存储数据库
+            String newFileName = UUID.randomUUID().toString()+suffixName;
+
+            String url=filePath + newFileName;
+            //创建文件
+            File dest = new File(url);
+            url=dest.getPath();
+            newinstitution.setDownloadUrl(url);
+            try {
+                file.transferTo(dest);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             institutionRepo.save(newinstitution);
             userRepo.save(newuser);
 
@@ -81,5 +105,14 @@ public class InstitutionController {
             }
         }
         return 0;
+    }
+
+    @GetMapping(value = "/institution/info")
+    public Institution getInstitution(@RequestParam("institution_id") int institution_id){
+        Institution institution=institutionRepo.findByInstitutionId(institution_id);
+        if(institution!=null){
+            return institution;
+        }
+        return null;
     }
 }
--- src/main/java/com/summer/demo/Repository/InstitutionRepository.java
+++ src/main/java/com/summer/demo/Repository/InstitutionRepository.java
@@ -7,4 +7,6 @@ import com.summer.demo.Entity.Institution;
 public interface InstitutionRepository extends JpaRepository<Institution,Integer> {
     @Query("select COUNT(u) from Institution u where u.institutionName=?1")
     public int getNumberOfInstitutionName(String institutionName);
+
+    public Institution findByInstitutionId(int institutionId);
 }
