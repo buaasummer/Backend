81708ce54330aed67d2b4fe45665da75d3db26d7
alex <1219296951@qq.com>
2018-07-01 16:17:43 +0800

    论文提交API

---
+++ src/main/java/com/summer/demo/AssitClass/BigPaper.java
@@ -0,0 +1,77 @@
+package com.summer.demo.AssitClass;
+
+import com.summer.demo.Entity.Author;
+import com.summer.demo.Entity.PersonalUser;
+
+import java.util.List;
+
+public class BigPaper {
+    private int paperId;
+
+    private String title;
+
+    private String  names;
+    private String organizations;
+    private String  paperAbstract;
+    //1代表录用，2代表修改后录用，3代表待审核，4代表未录用。
+    private int status;
+
+    //论文下载url
+    private String downloadUrl;
+
+    public int getPaperId() {
+        return paperId;
+    }
+
+    public void setPaperId(int paperId) {
+        this.paperId = paperId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public String getOrganizations() {
+        return organizations;
+    }
+
+    public void setOrganizations(String organizations) {
+        this.organizations = organizations;
+    }
+
+    public String getPaperAbstract() {
+        return paperAbstract;
+    }
+
+    public void setPaperAbstract(String paperAbstract) {
+        this.paperAbstract = paperAbstract;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDownloadUrl() {
+        return downloadUrl;
+    }
+
+    public void setDownloadUrl(String downloadUrl) {
+        this.downloadUrl = downloadUrl;
+    }
+}
--- src/main/java/com/summer/demo/Controller/PaperController.java
+++ src/main/java/com/summer/demo/Controller/PaperController.java
@@ -1,6 +1,7 @@
 package com.summer.demo.Controller;
 
 
+import com.summer.demo.AssitClass.BigPaper;
 import com.summer.demo.AssitClass.CustomizedAuthor;
 import com.summer.demo.AssitClass.CustomizedPaper;
 import com.summer.demo.Entity.Author;
@@ -11,13 +12,19 @@ import com.summer.demo.Repository.AuthorRepository;
 import com.summer.demo.Repository.MeetingRepository;
 import com.summer.demo.Repository.PaperRepository;
 import com.summer.demo.Repository.PersonalUserRepository;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin
@@ -35,39 +42,72 @@ public class PaperController {
     @Autowired
     AuthorRepository authorRepository;
 
+    String url="http://127.0.0.1:8081/";
+
     @GetMapping(value="/paper/display")
-    public List<Paper> paperDisplay(@RequestParam("meetingId") int meetingId, @RequestParam("userId") int uerId)
+    public List<BigPaper> paperDisplay(@RequestParam("meetingId") int meetingId, @RequestParam("userId") int uerId)
     {
         Meeting meeting=meetingRepository.findByMeetingId(meetingId);
         PersonalUser personalUser=personalUserRepository.findByUserId(uerId);
-        return paperRepository.findPapersByPersonalUserAndMeeting(personalUser,meeting);
+        List<Paper> paperList=paperRepository.findPapersByPersonalUserAndMeeting(personalUser,meeting);
+        List<BigPaper> bigPapers=new ArrayList<>();
+        for(int i=0;i<paperList.size();i++)
+        {
+            Paper paper=paperList.get(i);
+            BigPaper  bigPaper=new BigPaper();
+            bigPaper.setPaperId(paper.getPaperId());
+            bigPaper.setTitle(paper.getTitle());
+            bigPaper.setPaperAbstract(paper.getPaperAbstract());
+            bigPaper.setDownloadUrl(paper.getDownloadUrl());
+            bigPaper.setStatus(paper.getStatus());
+            String str=paper.getAuthorIds();
+            String[] strings=str.split(",");
+            String names="";
+            String organizations="";
+            for(int j=0;j<strings.length;j++)
+            {
+                int authorID=Integer.parseInt(strings[j]);
+                Author author=authorRepository.findByAuthorId(authorID);
+                if(j!=strings.length-1)
+                {
+                    names+=author.getAuthorName()+",";
+                    organizations+=author.getOrganization()+",";
+                }
+                else
+                {
+                    names+=author.getAuthorName();
+                    organizations+=author.getOrganization();
+                }
+            }
+            bigPaper.setNames(names);
+            bigPaper.setOrganizations(organizations);
+            bigPapers.add(bigPaper);
+        }
+        return bigPapers;
     }
-    @GetMapping(value="/getAuthor")
-    public Author getAuthor(@RequestParam("authorId") int authorId)
-    {
-        return authorRepository.findByAuthorId(authorId);
-    }
+//    @GetMapping(value="/getAuthor")
+//    public Author getAuthor(@RequestParam("authorId") int authorId)
+//    {
+//        return authorRepository.findByAuthorId(authorId);
+//    }
     @PostMapping(value ="/paper/submission")
-    public int paperSubmit(@RequestParam("userId") int userId,@RequestParam("meetingId")int meetingId,
+    public Boolean paperSubmit(@RequestParam("paperId") int paperId,
                             CustomizedPaper customizedPaper)
     {
-        Meeting meeting=meetingRepository.findByMeetingId(meetingId);
-        PersonalUser personalUser=personalUserRepository.findByUserId(userId);
-        Paper paper=new Paper();
-        paper.setMeeting(meeting);
-        paper.setPersonalUser(personalUser);
-        int paperId=paper.getPaperId();
+        Paper paper=paperRepository.getOne(paperId);
         String title=customizedPaper.getTitle();
         MultipartFile multipartFile=customizedPaper.getMultipartFile();
         String paperAbstract=customizedPaper.getPaperAbstract();
         paper.setTitle(title);
         paper.setPaperAbstract(paperAbstract);
         BufferedOutputStream stream;
+        String downloadUrl="";
         if(!multipartFile.isEmpty())
         {
             try {
                 byte[] bytes = multipartFile.getBytes();
-                String pathName="upload/paper"+multipartFile.getOriginalFilename();
+                String pathName="upload/paper/"+multipartFile.getOriginalFilename();
+                downloadUrl=url+"paper/"+multipartFile.getOriginalFilename();
                 File saveFile=new File(pathName);
                 stream = new BufferedOutputStream(new FileOutputStream(saveFile));
                 stream.write(bytes);
@@ -75,31 +115,41 @@ public class PaperController {
                 stream.close();
             } catch (Exception e) {
                 e.printStackTrace();
-                return -1 ;//提交论文
+                return false ;//提交论文失败
             }
         }
+        paper.setDownloadUrl(downloadUrl);
+        paper.setStatus(3);
         paperRepository.save(paper);
         System.out.println(paper.getPaperId());
-        return paperId;
+        return true;
     }
     @CrossOrigin
     @PostMapping(value = "/paper/author")
-    public int authorSubmit(@RequestParam("userId") int userId,@RequestParam("meetingId") int meetingId,
-                                List<CustomizedAuthor> customizedAuthorList)
+    public int authorSubmit(@RequestParam("userId") int userId, @RequestParam("meetingId") int meetingId,
+                            @RequestParam("customizedAuthorList") String str)
     {
+        System.out.println(str);
+        JSONArray jsonArray=JSONArray.fromObject(str);
+        Object[] customizedAuthorList=jsonArray.toArray();
         PersonalUser user=personalUserRepository.findByUserId(userId);
+        Meeting meeting=meetingRepository.findByMeetingId(meetingId);
         Paper paper=new Paper();
+        paper.setPersonalUser(user);
+        paper.setMeeting(meeting);
         String authorIds="";
+        System.out.println(customizedAuthorList.length);
         if(customizedAuthorList==null)
             return -1;
-        for(int i=0;i<customizedAuthorList.size();i++)
+        for(int i=0;i<customizedAuthorList.length;i++)
         {
             Author author=new Author();
-            author.setAuthorName(customizedAuthorList.get(i).getAuthorName());
-            author.setIdentificationNumber(customizedAuthorList.get(i).getIdentificationNumber());
-            author.setOrganization(customizedAuthorList.get(i).getOrganization());
+            JSONObject jsonObject=JSONObject.fromObject(customizedAuthorList[i]);
+            author.setAuthorName(jsonObject.getString("authorName"));
+            author.setIdentificationNumber(jsonObject.getInt("identificationNumber"));
+            author.setOrganization(jsonObject.getString("organization"));
             authorRepository.save(author);
-            if(i!=customizedAuthorList.size()-1)
+            if(i!=customizedAuthorList.length-1)
             authorIds+=author.getAuthorId()+",";
             else
                 authorIds+=author.getAuthorId()+"";
